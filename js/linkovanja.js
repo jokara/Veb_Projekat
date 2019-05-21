6f6f61c870f6e5590850ddb715e41bbb2ec381f5
Sara Milovanović <48448209+sara-milovanovic@users.noreply.github.com>
2019-05-22 00:18:29 +0200

    gotov front bez breadcrumbs

---
+++ js/linkovanja.js
@@ -0,0 +1,35 @@
+function baner2(){
+	window.open("https://wellnessmama.com/35192/lemon-water/");
+}
+
+function baner3(){
+	window.open("https://rhnewtown.com.au/properties-for-sale?gclid=Cj0KCQjww47nBRDlARIsAEJ34bkIA8hWoSrK60S2OH3C3h3TQXMIEb3HocJkvXzyHXx1t67vei5Z9zoaAoktEALw_wcB");
+}
+
+function baner4(){
+	window.open("https://softech-solutions.com/");
+}
+
+function baner5(){
+	window.open("https://www.foodnetwork.com/healthy/packages/healthy-every-week/healthy-mains/foodnetwork-most-saved-healthy-recipes");
+}
+
+function all(){
+	
+}
+
+function primeni_filter(){
+	
+}
+
+function potvrda_kupovine(){
+	
+}
+
+function otkazivanje_kupovine(){
+	
+}
+
+function stavi_u_korpu(){
+	//alert("stvaljeno");
+}
