2e75b5902907163d3baf86b6860606c8f8967780
Sara Milovanović <48448209+sara-milovanovic@users.noreply.github.com>
2019-05-27 01:58:18 +0200

    Update Ocene.js

--- js/Ocene.js
+++ js/Ocene.js
@@ -47,6 +47,28 @@ function racunaj(ime){
 		}
 		star+="<font size='5'> <br> "+prosek+"</font>";
 		document.getElementById('prosecna').innerHTML=star;
+		
+		/*switch(ime){
+		
+			case 'Baloro': ocena=parseInt(document.baloro_form.ocena.value); break;
+			case 'Bergamo': ocena=parseInt(document.bergamo_form.ocena.value); break;
+			case 'Saint': ocena=parseInt(document.saint_forma.ocena.value); break;
+			case 'Walter': ocena=parseInt(document.walter_form.ocena.value); break;
+			case 'Bw': ocena=parseInt(document.bw_form.ocena.value); break;
+			case 'Bucko': ocena=parseInt(document.bucko_forma.ocena.value); break;
+			case 'Cake': ocena=parseInt(document.cake_form.ocena.value); break;
+			case 'Caribic': ocena=parseInt(document.caribic_form.ocena.value); break;
+			case 'Dolce': ocena=parseInt(document.dolce_form.ocena.value); break;
+			case 'Good': ocena=parseInt(document.good_forma.ocena.value); break;
+			case 'Go': ocena=parseInt(document.go_form.ocena.value); break;
+			case 'Grill': ocena=parseInt(document.grill51_form.ocena.value); break;
+			case 'Krilca': ocena=parseInt(document.krilca_form.ocena.value); break;
+			case 'Marukoshi': ocena=parseInt(document.mar_form.ocena.value); break;
+			case 'Mc': ocena=parseInt(document.mc_form.ocena.value); break;
+			case 'Poke': ocena=parseInt(document.poke_form.ocena.value); break;
+			
+		}*/ //NAMESTITI UPDATE PROSEKA
+		
 	}
 	else{
 		var prosek=parseInt(localStorage.getItem('prosecna_'+ime));
